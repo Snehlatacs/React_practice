8cb1507d7c45442aa91553372880545c98657355
snehlatakvs29@gmail.com <snehlatakvs29@gamil.com>
2025-06-14 23:45:15 +0530

    📁 Added jsx-demo project – My first JSX-based React component

---
+++ jsx-demo/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Welcome from './components/Welcome'; // 👈 this is your component
+
+function App() {
+  return (
+    <div className="App">
+      <Welcome />
+    </div>
+  );
+}
+
+export default App;
---
+++ jsx-demo/src/components/Welcome.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Welcome() {
+  const name = "Snehlata";
+
+  return (
+    <div>
+      <h1>Hello, {name}!</h1>
+      <p>This is your first JSX component 🎉</p>
+    </div>
+  );
+}
+
+export default Welcome;
